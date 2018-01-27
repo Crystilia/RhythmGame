5b071fb9f140b714902824162630325fd112d064
PatAtUCF <35389689+PatAtUCF@users.noreply.github.com>
2018-01-27 15:14:14 -0500

    small programing update
    notes hurt you if missed

---
+++ DIG4720C-RhythmGame/Assets/Scripts/MissedNoteKill.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissedNoteKill : MonoBehaviour {
+    private Manager mngr;
+
+    // Use this for initialization
+    void Start () {
+        mngr = GameObject.Find("Manager").GetComponent<Manager>();
+	}
+    private void OnTriggerEnter(Collider note)
+    {
+        if(note.tag == "Note")
+        {
+            mngr.LowerHP();
+        }
+        Destroy(note.gameObject);
+    }
+}
