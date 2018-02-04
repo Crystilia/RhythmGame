9ae5877bc178aaf595e145af6f18a138f3616d5c
PatAtUCF <35389689+PatAtUCF@users.noreply.github.com>
2018-02-04 17:24:48 -0500

    char selectish
    spins

---
+++ DIG4720C-RhythmGame/Assets/Scripts/PlayerRotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerRotate : MonoBehaviour {
+    public float speed = 15f;
+    // Use this for initialization
+    void Start () {
+		if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("CharSelect"))
+        {
+            this.enabled = true;
+        }
+        else if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("CharSelect2P"))
+        {
+            this.enabled = true;
+        }
+        else
+        {
+            this.enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+	}
+}
