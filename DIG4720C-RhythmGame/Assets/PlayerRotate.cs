5904eee755d8fe4d141b296f5226f104b7c1d8f0
PatAtUCF <35389689+PatAtUCF@users.noreply.github.com>
2018-02-04 16:39:57 -0500

    started working on player select
    new scene buttons and script

---
+++ DIG4720C-RhythmGame/Assets/PlayerRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour {
+    public float speed = 15f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+	}
+}
