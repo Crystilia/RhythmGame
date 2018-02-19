36c90f74925b2abe91690210470b77050ea52d0b
PatAtUCF <35389689+PatAtUCF@users.noreply.github.com>
2018-02-18 23:14:15 -0500

    changing functionality
    char select overhaul for single/multiplayer/part1

--- DIG4720C-RhythmGame/Assets/Scripts/CharMenu.cs
+++ DIG4720C-RhythmGame/Assets/Scripts/CharMenu.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,13 +6,15 @@ public class CharMenu : MonoBehaviour {
     public GameObject[] Players;
     public static GameObject Player;
     Quaternion rot = new Quaternion(0, 165, 0,0);
+
     private void Start()
     {
             Players[0] = GameObject.Find("Player1");
             Players[1] = GameObject.Find("Player2");
             Players[2] = GameObject.Find("Player3");
             Players[3] = GameObject.Find("Player4");
-
+        
+    
         if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("CharSelect") || SceneManager.GetActiveScene() == SceneManager.GetSceneByName("MultiplayerCharSelect"))
         {
             PlayerButton(0);
@@ -23,10 +24,19 @@ public class CharMenu : MonoBehaviour {
         {
             PlayerButton(PlayerPrefs.GetInt("P"));
         }
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("MultiPlayer"))
+        {
+            PlayerButton(PlayerPrefs.GetInt("P"));
+            Player2Button(PlayerPrefs.GetInt("P2"));
+        }
     }
-    public void Char1()
+    public void SinglePlayerStart()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(3);
+    }
+    public void MultiPlayerStart()
+    {
+        SceneManager.LoadScene(4);
     }
     public void Backbutton()
     {
@@ -51,6 +61,27 @@ public class CharMenu : MonoBehaviour {
         if (PlayerPrefs.GetInt("P") != i)
         {
             PlayerPrefs.SetInt("P", i);
+        }
+    }
+    public void Player2Button(int i)
+    {
+        if (Player != null)
+        {
+            rot = Player.transform.rotation;
+        }
+        Player = Players[i];
+
+        for (int k = 0; k < Players.Length; k++)
+        {
+
+            Players[k].SetActive(false);
+        }
+        Player.SetActive(true);
+        Player.transform.rotation = rot;
+
+        if (PlayerPrefs.GetInt("P2") != i)
+        {
+            PlayerPrefs.SetInt("P2", i);
         }
     }
 }
