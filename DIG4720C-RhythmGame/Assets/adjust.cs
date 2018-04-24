dfd5e80a8883435d137b40dfec381319ed0a76e9
PatAtUCF <35389689+PatAtUCF@users.noreply.github.com>
2018-04-24 18:32:25 -0400

    change sensor core settings
    arrow keys

---
+++ DIG4720C-RhythmGame/Assets/adjust.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class adjust : MonoBehaviour {
+
+    public SensorCore SC;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            SC.flexThreshold = SC.flexThreshold - 0.01f;
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            SC.flexThreshold = SC.flexThreshold + 0.01f;
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            SC.restThreshold = SC.restThreshold + 0.01f;
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            SC.restThreshold = SC.restThreshold - 0.01f;
+        }
+    }
+}
